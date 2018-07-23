e09c275dd6a197468546f07f9057d231f636fef2
Kyle <k032492@gmail.com>
2018-07-22 21:14:01 -0400

    game mode added

--- keyboards/dz60/keymaps/mainian/keymap.c
+++ keyboards/dz60/keymaps/mainian/keymap.c
@@ -14,6 +14,7 @@ enum layers {
   _S2,
   _F2,        // numpad layer
   _AL,        // Adjust Layer
+  _GL,        // Gaming Layer
 };
 
 #define _______ KC_TRNS
@@ -31,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 * |-----------------------------------------------------------------------------------------+
 * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |      Shift      |
 * |-----------------------------------------------------------------------------------------+
-* | Ctrl  |  GUI |  Alt |    _S2    | _SL |      Space      |  Alt  |  _FL  | Fn2  |  Ctrl  |
+* | Ctrl  |  GUI |  Alt |    _S2    | _SL |      Space      |  Alt  |  _FL  | _F2  |  Ctrl  |
 * `-----------------------------------------------------------------------------------------'
 */
 
@@ -65,10 +66,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_F2] = KEYMAP(
 		KC_TRNS,  KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_DEL, 
-		KC_CAPS, KC_VOLU, KC_MUTE, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, RESET, 
+		KC_CAPS, KC_VOLU, KC_MUTE, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, TG(_GL), 
     KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_F5, KC_F6, KC_F7, KC_F8, KC_TRNS, KC_TRNS, 
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    
+  [_GL] = KEYMAP(
+		KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_NO, KC_BSPC, 
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, 
+		KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, 
+		KC_LSFT, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_NO, 
+		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(_SL), KC_SPC, KC_RALT, MO(_FL), KC_NO, MO(_F2), TG(_GL)),
 };
 
 void persistent_default_layer_set(uint16_t default_layer) {
@@ -98,6 +106,9 @@ void matrix_scan_user(void) {
         break;
       case _F2:
         rgblight_setrgb(0xFF, 0x81, 0x00);
+        break;
+      case _GL:
+        rgblight_setrgb(0xFF, 0xFF, 0xFF);
         break;
       case _BL:
       default:
